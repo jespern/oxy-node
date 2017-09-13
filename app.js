069c53e9d2b2274068f9d7030e0523d357c9afba
Andrea Baccega <vekexasia@gmail.com>
2017-09-13 08:27:46 +0200

    gitignore eslint and final merge

--- app.js
+++ app.js
@@ -544,6 +544,7 @@ d.run(function () {
 					var apiEndpointPath = config.api[moduleName][protocol];
 					try {
 						var ApiEndpoint = require(apiEndpointPath);
+						new ApiEndpoint(scope.modules[moduleName], scope.network.app, scope.logger, scope.modules.cache);
 					} catch (e) {
 						scope.logger.error('Unable to load API endpoint for ' + moduleName + ' of ' + protocol, e);
 					}
